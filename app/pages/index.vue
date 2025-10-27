f64175b4b791b5186fcdbd0f0f8773eee27d45b4
George Roe <george.roe@stfc.ac.uk>
2025-10-27 20:55:07 +0000

    Add about me section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <section class="pb-12 pt-16">
     <name-carousel />
-  </div>
+  </section>
+  <hr class="border-neutral-800 mx-12" />
+  <section class="container mx-auto px-2 py-6">
+    <p>Hi, I am <span class="font-bold">George Roe</span>, a software engineer working at the <a href="https://www.ukri.org/councils/stfc/">Science and Technology Facilities Council</a>. <span class="font-bold">I enjoy improving others lives through technology and software.</span></p>
+  </section>
+  <hr class="border-neutral-800 mx-12" />
 </template>
