a9317f5fa5321d06d07f6db53bd75cc6febadab5
George Roe <george.roe@stfc.ac.uk>
2025-10-28 13:13:56 +0000

    Ensure website doesnt appear off screen on iphone

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full h-screen font-sf-mono bg-black text-white overflow-x-hidden overflow-y-auto"
+    class="w-full min-h-screen min-h-[100dvh] font-sf-mono bg-black text-white overflow-x-hidden overflow-y-auto"
   >
     <nuxt-page />
   </div>
